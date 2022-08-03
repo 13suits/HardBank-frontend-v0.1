67d110a73581aa415f062f2bceed1c520977619a
thirteensuits <110221153+thirteensuits@users.noreply.github.com>
2022-08-03 17:49:34 +0900

    Update Buy.jsx

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -14,16 +14,14 @@ function Buy() {
             <h1 class="font-weight-bold">Buy</h1>
             <br></br>
             <p>
-            The idea is for the purchase of a product to be executed by minting from an NFT smart contract address such that each purchase is represented 1-to-1 by an NFT on the blockchain.
+            To purchase a given product through the HardBank protocol, we execute a mint from its NFT smart contract address.
             <br></br>
-            Each purchase is uniquely defined by its NFT's index number.
+            Each purchase is consequently represented 1-to-1 by an NFT on the blockchain and uniquely defined by the minted NFT's index number.
             <br></br>
             <br></br>
-            To provide an example, let's say we are selling (physical) branches:
+            In this demo, we are selling (physical) branches for <b>0.001 ETH</b>.
             <br></br>
             To purchase a branch from us, you would mint a BranchNFT from the BranchNFT smart contract address.
-            <br></br>
-            For the purposes of this Demo, let's set the price of each branch to <b>0.001 ETH</b>.
             <br></br>
             <br></br>
             Try it out! Connect your wallet and purchase a Branch -- and remember, we are operating on the Rinkeby testnet!
@@ -34,7 +32,6 @@ function Buy() {
             </Provider>
             <br></br>
             After purchasing, please head to the <b>Claim</b> page to claim your order.
-              <br></br>
               <br></br>
               <br></br>
               <br></br>
